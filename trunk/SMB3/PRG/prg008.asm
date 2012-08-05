e0419adf140010d23587fb5f12b2e6bf61da4de8
dahrkdaiz@gmail.com <dahrkdaiz@gmail.com@b3dc581e-a07d-6514-0d16-446ec0051f41>
2012-08-05 10:31:55 +0000

    -Fireball animation fixed -disable controller input during fireball mode

--- trunk/SMB3/PRG/prg008.asm
+++ trunk/SMB3/PRG/prg008.asm
@@ -1313,6 +1313,14 @@ PRG008_A6DA:
 	STA <Pad_Input		; Otherwise, disable 'B' button
 
 PRG008_A6E5:
+	LDA Fox_FireBall		; if in fireball mode, disable, all but select and start
+	BEQ Normal_008_1
+
+	LDA <Pad_Input		
+	AND #(PAD_START | PAD_SELECT)
+	STA <Pad_Input
+
+Normal_008_1:
 	LDA Level_Objects+1
 	CMP #OBJ_TOADANDKING
 	BNE PRG008_A6F2	 	; If first object is not "Toad and the King" (i.e. we're in the end of world castle), jump to PRG008_A6F2
